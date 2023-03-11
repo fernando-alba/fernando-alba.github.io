8d19c7d73cd1c965ea22284417a2418c8b10dd95
alessandro-bencini <info@alessandro-bencini.com>
2023-03-11 15:46:25 +0100

    Code rafactor

---
+++ docs/assets/static/singles.ts
@@ -0,0 +1,100 @@
+import { Release } from './types';
+
+// Per aggiungere un singolo, in fondo, duplicare l'ultimo blocco tra due parentesi graffe e compilare tutti i campi avendo cura di
+// incrementare di +1 il campo id.
+
+export const singles: Release[] = [
+  {
+    id: 1,
+    title: 'Favola',
+    year: '2014',
+    image: 'lcn.png',
+    spotify:
+      'https://open.spotify.com/album/2X6ah5b8Ot3hhg76gCLoyw?si=ew9bOC2YQ92WLrY5atY4YA',
+    apple:
+      'https://music.apple.com/it/album/favola-feat-stefania-orlando-single/1462917445',
+    amazon:
+      'https://music.amazon.it/albums/B07RG147T2?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_CH6a1sn4R6Dj6b9pCSfLnsJY3&trackAsin=B07RF38FJQ',
+  },
+  {
+    id: 2,
+    title: 'Prima di lunedì',
+    year: '2016',
+    image: 'pdl-single.jpg',
+    spotify:
+      'https://open.spotify.com/album/2EIFD65DVWNmbzwsugYbJS?si=YTHif2tRSMO_GNRWJJ72Vw',
+    apple:
+      'https://music.apple.com/it/album/prima-di-luned%C3%AC-feat-stefania-orlando-single/1155074905',
+    amazon:
+      'https://music.amazon.it/albums/B01M99FJFS?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_i5CPepWLNzfS4IPsT1zSQlnDo',
+  },
+  {
+    id: 3,
+    title: 'Pensiero fisso',
+    year: '2020',
+    image: 'pensiero-fisso.jpg',
+    spotify:
+      'https://open.spotify.com/album/3qxj4TAU7crFSRwuBIPtEz?si=BfX-JaeVSAqeN0s1IArluA',
+    apple: 'https://music.apple.com/it/album/pensiero-fisso-single/1510331660',
+    amazon:
+      'https://music.amazon.it/albums/B087R6MD8J?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_x6ihMwN5O9R5lvCUOSsJXUsmw&trackAsin=B087R52R4N',
+  },
+  {
+    id: 4,
+    title: 'Abbiamo contagiato il mondo',
+    year: '2020',
+    image: 'acm.jpg',
+    spotify:
+      'https://open.spotify.com/album/1zq0qYAZRGYmUGyGXrucxZ?si=NgXdqDyRSWW028dlf5RWOA',
+    apple:
+      'https://music.apple.com/it/album/abbiamo-contagiato-il-mondo-single/1538179007',
+    amazon:
+      'https://music.amazon.it/albums/B08MBHPYG4?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_RkRN7AJa27kUDjW0gsQ9oGQ7I',
+  },
+  {
+    id: 5,
+    title: 'Immagina',
+    year: '2020',
+    image: 'immagina.jpg',
+    spotify:
+      'https://open.spotify.com/album/1P4Tv1M3CDmzBITwyb914Z?si=L-Ti-JpFQHS2_XE-JMpB5A',
+    amazon:
+      'https://music.amazon.it/albums/B08TQSN7PC?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_kIkmny2aSKUBiKw2PCzuLMeou&trackAsin=B08TR782GG',
+  },
+  {
+    id: 6,
+    title: "L'odore dell'amore",
+    year: '2021',
+    image: 'oda.jpg',
+    spotify:
+      'https://open.spotify.com/album/7FuDO5ipee9ghkrb52KhgO?si=hVXYgYrvS0qsbkRoYkd8Ig',
+    apple:
+      'https://music.apple.com/it/album/lodore-dellamore-single/1562164264',
+    amazon:
+      'https://music.amazon.it/albums/B09234L48K?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_8xoRyKToyV543AUs1dplvJkjL',
+  },
+  {
+    id: 7,
+    title: 'La canzone del Mondo',
+    year: '2022',
+    image: 'cdm.jpg',
+    spotify:
+      'https://open.spotify.com/album/1jhpYauuaruVu78cGk7A5e?si=u0BUGkMcQ_SCgM9U0zRjcg',
+    apple:
+      'https://music.apple.com/it/album/la-canzone-del-mondo-single/1619136319',
+    amazon:
+      'https://music.amazon.it/albums/B09XWZMGZC?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_U0HRJhdfrABJVr8QRJhmqvttH',
+  },
+  {
+    id: 8,
+    title: 'Il Sole e la Luna',
+    year: '2022',
+    image: 'sole-luna.png',
+    spotify:
+      'https://open.spotify.com/album/2qdMmYXpA2vbFdVNzW6xxN?si=dewWdcolRxqiUohUr8vJtA',
+    apple:
+      'https://music.apple.com/it/album/il-sole-e-la-luna-single/1649200804',
+    amazon:
+      'https://music.amazon.it/albums/B0BHWPFFSQ?marketplaceId=APJ6JRA9NG5V4&musicTerritory=IT&ref=dm_sh_KZDx0e12uMeC9UtN0b54WxxCk&trackAsin=B0BHWNM73N',
+  },
+];
